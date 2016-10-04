ed4bfc8ad1bac05da0d34ce4c5f3bb9ed1a7de0e
Ezard <ezard.ben@gmail.com>
2016-10-04 01:09:02 +0100

    Add missing require statement

--- github.js
+++ github.js
@@ -3,6 +3,7 @@ var createHandler = require('github-webhook-handler');
 var handler = createHandler({path: '/github/release', secret: '84aG0M9XcJdc1bryIDPz'});
 var GitHubApi = require('github');
 var git = require('simple-git');
+var exec = require('child_process').exec;
 
 var github = new GitHubApi({
 	Promise: require('bluebird')
