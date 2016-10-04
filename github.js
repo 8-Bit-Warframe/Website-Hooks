0145220168e6211bde878109f8504bc19de961d9
Ezard <ezard.ben@gmail.com>
2016-10-04 01:00:14 +0100

    Update to use GitHub API to get releases' commit hash

--- github.js
+++ github.js
@@ -1,13 +1,34 @@
-var express = require('express');
-var git = require('simple-git')();
-var exec = require('child_process').exec;
+var http = require('http');
+var createHandler = require('github-webhook-handler');
+var handler = createHandler({path: '/github/release', secret: '84aG0M9XcJdc1bryIDPz'});
+var GitHubApi = require('github');
+var git = require('simple-git');
 
-var app = express();
-
-app.post('/github/release', function () {
-	git.pull("origin", "master");
-	git.submoduleUpdate();
-	exec("gradle createExe", {cwd: '/ls-src/desktop'});
+var github = new GitHubApi({
+	Promise: require('bluebird')
 });
 
-app.listen(1234);
+http.createServer(function (req, res) {
+	handler(req, res, function () {
+		res.statusCode = 404;
+		res.end('No such location')
+	})
+}).listen(1234);
+
+handler.on('error', function (err) {
+	console.error('Error: ', err.message)
+});
+
+handler.on('release', function (event) {
+	github.gitdata.getReference({
+		user: '8-Bit-Warframe',
+		repo: 'Lost-Sector-desktop',
+		ref: 'tags/' + event.payload.release.tag_name
+	}, function (err, res) {
+		git.checkout("master");
+		git.pull("origin", "master");
+		git.checkout(res.object.sha);
+		git.submoduleUpdate();
+		exec("gradle createExe", {cwd: '/ls-src/desktop'});
+	});
+});
