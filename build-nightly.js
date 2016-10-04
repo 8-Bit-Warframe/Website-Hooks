0bab785ea2bd317e9ffc880e3839beb99abf67d3
Ezard <ezard.ben@gmail.com>
2016-10-04 19:28:30 +0100

    Remove erroneous submodule update

--- build-nightly.js
+++ build-nightly.js
@@ -3,6 +3,5 @@ var exec = require('child_process').exec;
 
 git.checkout("master");
 git.pull("origin", "master");
-git.submoduleUpdate();
 exec("gradle shadowJar", {cwd: '/ls-src/desktop'});
 exec("gradle createExe", {cwd: '/ls-src/desktop'});
