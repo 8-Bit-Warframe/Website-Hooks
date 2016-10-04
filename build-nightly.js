d8859c419f8043529c2e3236b91a9bb64aeba0d4
Ezard <ezard.ben@gmail.com>
2016-10-04 17:03:36 +0100

    Add nightly build file

---
+++ build-nightly.js
@@ -0,0 +1,8 @@
+var git = require('simple-git');
+var exec = require('child_process').exec;
+
+git.checkout("master");
+git.pull("origin", "master");
+git.submoduleUpdate();
+exec("gradle shadowJar", {cwd: '/ls-src/desktop'});
+exec("gradle createExe", {cwd: '/ls-src/desktop'});
