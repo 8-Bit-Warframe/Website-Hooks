36adb523c667607e122d7c105fa9d6d82ec399ea
Ezard <ezard.ben@gmail.com>
2016-10-15 05:29:33 +0100

    Removed usage of simple-git module

--- build-nightly.js
+++ build-nightly.js
@@ -1,7 +1,14 @@
-var git = require('simple-git')();
 var execSync = require('child_process').execSync;
 
-git.checkout("master");
-git.pull("origin", "master");
+console.log("Starting nightly build...");
+console.log("Checking out master...");
+execSync("git checkout master", {cwd: '/ls-src/desktop'});
+console.log("Pulling from master...");
+execSync("git pull origin master", {cwd: '/ls-src/desktop'});
+console.log("Updating submodules...");
+execSync("git submodule update", {cwd: '/ls-src/desktop'});
+console.log("Building jar...");
 execSync("gradle shadowJar -PversionText=NIGHTLY", {cwd: '/ls-src/desktop'});
+console.log("Building exe...");
 execSync("gradle createExe -PversionText=NIGHTLY", {cwd: '/ls-src/desktop'});
+console.log("Nightly build complete");
